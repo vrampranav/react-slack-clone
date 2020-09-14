6c3fbf5a25277325827bec131cfea27f2ee5fbb7
Rampranav <vrampranav9@gamil.com>
2020-09-14 21:23:02 +0530

    Added SignIn Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import App from './App'
+import SignIn from './SignIn';
 
 export{
-    App
+    App,
+    SignIn
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './components'; //import Obj from './components'
+import {App,SignIn} from './components'; //import Obj from './components'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+ReactDOM.render(<SignIn/>,document.getElementById('root'));
